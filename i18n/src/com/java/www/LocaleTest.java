2426bd1db65ebedc92960e11d4a29578126e317a
cucker <hanxiao2100@qq.com>
2020-06-08 20:52:48 +0800

    add i18n国际化

---
+++ i18n/src/com/java/www/LocaleTest.java
@@ -0,0 +1,29 @@
+package com.java.www;
+
+import org.junit.Test;
+
+import java.util.Locale;
+
+public class LocaleTest {
+    @Test
+    public void getAllLocales() {
+        Locale[] locales = Locale.getAvailableLocales();
+        // 打印所有的地区信息
+        for (Locale locale : locales) {
+            System.out.println(locale);
+        }
+    }
+
+    @Test
+    public void getDefaultLocale() {
+        Locale locale = Locale.getDefault();
+        System.out.println(locale);  // zh_CN
+    }
+
+    @Test
+    public void createLocale() {
+        // new Locale(String language, String country);
+        Locale locale = new Locale("zh", "CN");
+        System.out.println(locale);
+    }
+}
